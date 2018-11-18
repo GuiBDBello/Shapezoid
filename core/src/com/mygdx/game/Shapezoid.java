bcb7047244ad0b3c579cade7919c331df50600e8
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2018-11-18 20:44:27 -0200

    Created a simple 3D scene - Still testing

--- core/src/com/mygdx/game/Shapezoid.java
+++ core/src/com/mygdx/game/Shapezoid.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Environment;
@@ -18,126 +19,140 @@ import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Vector3;
 
 public class Shapezoid extends ApplicationAdapter implements InputProcessor {
+
     // PerspectiveCamera é usada para 3D, possui profundidade;
-    private PerspectiveCamera camera;
+    private PerspectiveCamera perspectiveCamera;
     // Guarda vários modelos, carrega mais rapidamente que modelos individuais, pois realiza apenas uma chamada;
     private ModelBatch modelBatch;
     // Cria os modelis;
     private ModelBuilder modelBuilder;
     // Objeto 3D;
-    private Model box;
+    private Model player;
+    private Model enemyTriangle;
+    private Model enemySquare;
     // Instância de um Model;
     private ModelInstance modelInstance;
     // Gerencia shaders, luzes, etc.;
     private Environment environment;
-    
+
     public Vector3 origin = new Vector3(0f, 0f, 0f);
-    
+
     public Vector3 axisX = new Vector3(1f, 0f, 0f);
     public Vector3 axisY = new Vector3(0f, 1f, 0f);
     public Vector3 axisZ = new Vector3(0f, 0f, 1f);
-    
+
     @Override
     public void create() {
-        
         // Cria a câmera com um campo de visão e tamanho da tela;
-        camera = new PerspectiveCamera(75, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        perspectiveCamera = new PerspectiveCamera(75, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         // Define a posição da câmera (3 afastado em Z da origem);
-        camera.position.set(0f, 0f, 3f);
+        perspectiveCamera.position.set(0f, 0f, 3f);
         // Destino que a câmera está apontando (origem);
-        camera.lookAt(0f, 0f, 0f);
+        perspectiveCamera.lookAt(0f, 0f, 0f);
         // Menor distância que a câmera "captura" (View Distance);
-        camera.near = 0.1f;
+        perspectiveCamera.near = 0.1f;
         // Maior distância que a câmera "captura" (View Distance);
-        camera.far = 300f;
-        
+        perspectiveCamera.far = 300f;
+
         modelBatch = new ModelBatch();
         modelBuilder = new ModelBuilder();
-        
+
         // Cria um material, definindo seu tamanho (X, Y e Z, material e atributos (posição e normal);
-        box = modelBuilder.createBox(2f, 2f, 2f, 
-                new Material(ColorAttribute.createDiffuse(Color.BLUE)), 
+        player = modelBuilder.createSphere(2f, 2f, 2f, 100, 100, 
+                new Material(ColorAttribute.createDiffuse(Color.BLUE)),
                 VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
         // Cria uma instância de um Objeto 3D;
-        modelInstance = new ModelInstance(box, 0f, 0f, 0f);
+        modelInstance = new ModelInstance(player, 0f, 0f, 0f);
         
+        enemyTriangle = modelBuilder.createRect(0f, 1f, 2f, 0f, 1f, 2f, 0f, 1f, 2f, 2f, 2f, 2f, 2f, 2f, 2f, 
+                new Material(ColorAttribute.createDiffuse(Color.RED)), 
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        //modelInstance = new ModelInstance(enemyTriangle, 0f, 0f, 0f);
+        
+        enemySquare = modelBuilder.createBox(2f, 2f, 2f, 
+                new Material(ColorAttribute.createDiffuse(Color.RED)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        //modelInstance = new ModelInstance(enemySquare, 5f, 0f, -5f);
+
         // Cria o ambiente do jogo;
         environment = new Environment();
         // Define a luz ambiente (cor RGB e intensidade);
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.8f, 0.8f, 1f));
-        
+
         // Determina que o processamento de inputs acontece nessa classe;
         Gdx.input.setInputProcessor(this);
     }
 
     @Override
     public void render() {
+        Gdx.gl20.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT|GL20.GL_DEPTH_BUFFER_BIT);
-        
-        camera.update();
-        modelBatch.begin(camera);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        if (Gdx.input.isTouched()) {
+            System.out.println("Input occurred at x=" + Gdx.input.getX() + ", y=" + Gdx.input.getY());
+        }
+
+        perspectiveCamera.update();
+        modelBatch.begin(perspectiveCamera);
         modelBatch.render(modelInstance, environment);
         modelBatch.end();
-    }
-    
-    // 
-    private void disposeAll() {
-        
     }
 
     @Override
     public boolean keyDown(int keycode) {
         if (keycode == Input.Keys.LEFT) {
+            System.out.println("Tico");
             // Rotaciona a câmera em um ponto, por um eixo, ângulo de rotação;
-            camera.rotateAround(this.origin, this.axisY, 1f);
+            perspectiveCamera.rotateAround(this.origin, this.axisY, 1f);
         }
         if (keycode == Input.Keys.RIGHT) {
+            System.out.println("Bunda");
             // Rotaciona a câmera em um ponto, por um eixo, ângulo de rotação;
-            camera.rotateAround(this.origin, this.axisY, -1f);
+            perspectiveCamera.rotateAround(this.origin, this.axisY, -1f);
         }
         return true;
     }
 
     @Override
     public boolean keyUp(int i) {
-        
+
         return true;
     }
 
     @Override
     public boolean keyTyped(char c) {
-        
+
         return true;
     }
 
     @Override
     public boolean touchDown(int i, int i1, int i2, int i3) {
-        
+
         return true;
     }
 
     @Override
     public boolean touchUp(int i, int i1, int i2, int i3) {
-        
+
         return true;
     }
 
     @Override
     public boolean touchDragged(int i, int i1, int i2) {
-        
+
         return true;
     }
 
     @Override
     public boolean mouseMoved(int i, int i1) {
-        
+
         return true;
     }
 
     @Override
     public boolean scrolled(int i) {
-        
+
         return true;
     }
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,12 +1,17 @@
 package com.mygdx.game.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.Shapezoid;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Shapezoid(), config);
-	}
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Shapezoid";
+        config.width = 1280;
+        config.height = 720;
+        new LwjglApplication(new Shapezoid(), config);
+    }
 }
