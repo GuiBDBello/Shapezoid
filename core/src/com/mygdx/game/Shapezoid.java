b3e253981fe44d74d9c6e63597a65129cb8f2759
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2018-11-21 23:05:27 -0200

    Messing with camera

--- core/src/com/mygdx/game/Shapezoid.java
+++ core/src/com/mygdx/game/Shapezoid.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -28,18 +29,28 @@ public class Shapezoid implements Screen, InputProcessor {
 
     // PerspectiveCamera é usada para 3D, possui profundidade;
     private PerspectiveCamera perspectiveCamera;
+    
     // Guarda vários modelos, carrega mais rapidamente que modelos individuais, pois realiza apenas uma chamada;
     private ModelBatch modelBatch;
+    
     // Cria os modelis;
     private ModelBuilder modelBuilder;
+    
     // Objeto 3D;
     private Model player;
     private Model enemyTriangle;
     private Model enemySquare;
+    
     // Instância de um Model;
     private ModelInstance modelInstance;
+    private ModelInstance modelInstance2;
+    //private ModelInstance modelInstance3;
+    
     // Gerencia shaders, luzes, etc.;
     private Environment environment;
+    
+    // Sons do jogo;
+    private Music backgroundMusic;
 
     public Vector3 origin = new Vector3(0f, 0f, 0f);
 
@@ -51,16 +62,19 @@ public class Shapezoid implements Screen, InputProcessor {
         this.game = game;
 
         // Cria a câmera com um campo de visão e tamanho da tela;
-        perspectiveCamera = new PerspectiveCamera(75, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        perspectiveCamera = new PerspectiveCamera(60, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         // Define a posição da câmera (3 afastado em Z da origem);
-        perspectiveCamera.position.set(0f, 0f, 3f);
+        perspectiveCamera.position.set(0f, 5f, 10f);
         // Destino que a câmera está apontando (origem);
         perspectiveCamera.lookAt(0f, 0f, 0f);
         // Menor distância que a câmera "captura" (View Distance);
         perspectiveCamera.near = 0.1f;
         // Maior distância que a câmera "captura" (View Distance);
         perspectiveCamera.far = 300f;
+        // Atualiza a câmera;
+        perspectiveCamera.update();
 
+        // Cria o lote e o construtor de materiais;
         modelBatch = new ModelBatch();
         modelBuilder = new ModelBuilder();
 
@@ -71,20 +85,24 @@ public class Shapezoid implements Screen, InputProcessor {
         // Cria uma instância de um Objeto 3D;
         modelInstance = new ModelInstance(player, 0f, 0f, 0f);
 
-        enemyTriangle = modelBuilder.createRect(0f, 1f, 2f, 0f, 1f, 2f, 0f, 1f, 2f, 2f, 2f, 2f, 2f, 2f, 2f,
-                new Material(ColorAttribute.createDiffuse(Color.RED)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-        //modelInstance = new ModelInstance(enemyTriangle, 0f, 0f, 0f);
-
         enemySquare = modelBuilder.createBox(2f, 2f, 2f,
                 new Material(ColorAttribute.createDiffuse(Color.RED)),
                 VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-        //modelInstance = new ModelInstance(enemySquare, 5f, 0f, -5f);
+        modelInstance2 = new ModelInstance(enemySquare, 5f, 0f, -5f);
+
+        enemyTriangle = modelBuilder.createRect(0f, 1f, 2f, 0f, 1f, 2f, 0f, 1f, 2f, 2f, 2f, 2f, 2f, 2f, 2f,
+                new Material(ColorAttribute.createDiffuse(Color.RED)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        //modelInstance3 = new ModelInstance(enemyTriangle, 0f, 0f, 0f);
 
         // Cria o ambiente do jogo;
         environment = new Environment();
         // Define a luz ambiente (cor RGB e intensidade);
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.8f, 0.8f, 1f));
+
+        //backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal(""));
+        // Define se a música tocará em loop;
+        //backgroundMusic.setLooping(true);
 
         // Determina que o processamento de inputs acontece nessa classe;
         Gdx.input.setInputProcessor(this);
@@ -102,12 +120,12 @@ public class Shapezoid implements Screen, InputProcessor {
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            System.out.println("render Tico");
+            System.out.println("render left");
             // Rotaciona a câmera em um ponto, por um eixo, ângulo de rotação;
             perspectiveCamera.rotateAround(this.origin, this.axisY, 1f);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            System.out.println("render Bunda");
+            System.out.println("render right");
             // Rotaciona a câmera em um ponto, por um eixo, ângulo de rotação;
             perspectiveCamera.rotateAround(this.origin, this.axisY, -1f);
         }
@@ -115,11 +133,14 @@ public class Shapezoid implements Screen, InputProcessor {
         perspectiveCamera.update();
         modelBatch.begin(perspectiveCamera);
         modelBatch.render(modelInstance, environment);
+        modelBatch.render(modelInstance2, environment);
         modelBatch.end();
     }
 
     @Override
     public void show() {
+        // Inicia a música quando a tela é carregada;
+        //backgroundMusic.play();
     }
 
     @Override
@@ -140,18 +161,23 @@ public class Shapezoid implements Screen, InputProcessor {
 
     @Override
     public void dispose() {
+        player.dispose();
+        enemySquare.dispose();
+        enemyTriangle.dispose();
+        modelBatch.dispose();
+        backgroundMusic.dispose();
     }
 
     // INTERFACE INPUTPROCESSOR;
     @Override
     public boolean keyDown(int keycode) {
         if (keycode == Input.Keys.LEFT) {
-            System.out.println("keyDown Tico");
+            System.out.println("keyDown left");
             // Rotaciona a câmera em um ponto, por um eixo, ângulo de rotação;
             perspectiveCamera.rotateAround(this.origin, this.axisY, 1f);
         }
         if (keycode == Input.Keys.RIGHT) {
-            System.out.println("keyDown Bunda");
+            System.out.println("keyDown right");
             // Rotaciona a câmera em um ponto, por um eixo, ângulo de rotação;
             perspectiveCamera.rotateAround(this.origin, this.axisY, -1f);
         }
@@ -160,43 +186,36 @@ public class Shapezoid implements Screen, InputProcessor {
 
     @Override
     public boolean keyUp(int i) {
-
         return true;
     }
 
     @Override
     public boolean keyTyped(char c) {
-
         return true;
     }
 
     @Override
     public boolean touchDown(int i, int i1, int i2, int i3) {
-
         return true;
     }
 
     @Override
     public boolean touchUp(int i, int i1, int i2, int i3) {
-
         return true;
     }
 
     @Override
     public boolean touchDragged(int i, int i1, int i2) {
-
         return true;
     }
 
     @Override
     public boolean mouseMoved(int i, int i1) {
-
         return true;
     }
 
     @Override
     public boolean scrolled(int i) {
-
         return true;
     }
 }
